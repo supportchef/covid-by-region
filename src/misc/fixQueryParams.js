02c94ff739ee07530900e128d8837d7c6ca05dce
Sean Keenan <sean9keenan@gmail.com>
2020-03-31 00:36:42 -0700

    Fix start date sharing issue

--- src/misc/fixQueryParams.js
+++ src/misc/fixQueryParams.js
@@ -1,7 +1,13 @@
+import moment from 'moment';
+
 export default function (queryParams) {
   const newQueryParams = {};
   Object.entries(queryParams).forEach(([key, value]) => {
-    if (typeof value === 'string') {
+    if (key === 'startDate' && value !== null) {
+      newQueryParams[key] = moment(
+        decodeURIComponent(value).replace(/\"/g, '')
+      );
+    } else if (typeof value === 'string') {
       newQueryParams[key] = decodeURIComponent(value);
     } else if (key === 'pinnedKeys') {
       newQueryParams[key] = new Map(value);
